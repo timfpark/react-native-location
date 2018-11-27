cedf8acfeb6d84f2e7a90d4c1d2b9d650660b106
Matt Oakes <hello@mattoakes.net>
2018-11-27 13:49:31 +0000

    Remove the old type definitions

--- src/types.ts
+++ src/types.ts
@@ -4,4 +4,27 @@ export type AuthorizationStatus =
   | "denied"
   | "restricted"
   | "notDetermined"
+
+export interface Location {
+  coords: LocationCoords,
+  timestamp: number,
+}
+
+export interface LocationCoords {
+  latitude: number,
+  longitude: number,
+  altitude: number,
+  accuracy: number,
+  altitudeAccuracy: number,
+  course: number,
+  speed: number,
+  floor: number,
+}
+
+export interface Heading {
+  heading: number,
+}
+
 export type AuthorizationStatusCallback = (authorizationStatus: AuthorizationStatus) => void
+export type LocationCallback = (location: Location) => void
+export type HeadingCallback = (heading: Heading) => void
