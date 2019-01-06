382414c6161894df1fab7d1419f3c99bc246a0d4
Matt Oakes <hello@mattoakes.net>
2019-01-06 11:03:35 +0000

    WIP: Android jet detox testing

--- tests/integration/android/app/src/main/java/com/rnlocationtest/MainApplication.java
+++ tests/integration/android/app/src/main/java/com/rnlocationtest/MainApplication.java
@@ -2,11 +2,15 @@ package com.rnlocationtest;
 
 import android.app.Application;
 
+import io.invertase.jet.JetPackage;
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.github.reactnativecommunity.location.RNLocationPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNLocationPackage(),
+          new JetPackage()
       );
     }
 
